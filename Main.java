610e660d48aefb0dba5f2dc661cb3c94731ac049
OlgaNechay <olganechay96@gmail.com>
2017-05-29 01:52:54 +0300

    .

--- Main.java
+++ Main.java
@@ -1,31 +1,39 @@
 package com.company;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
-import javax.swing.*;
+
 
 public class Main {
-
-    //BufferedImage image;
     public static void main(String[] args) {
         try {
-            BufferedImage image = ImageIO.read(new File("y_int2_1_2_.png"));
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+            Mat source = Imgcodecs.imread("y_1pl1.png",
+                    Imgcodecs.CV_LOAD_IMAGE_COLOR);
+            Mat destination = new Mat(source.rows(), source.cols(), source.type());
+            //Размытие с Гауссовым фильтром
+            Imgproc.GaussianBlur(source, destination, new Size(7, 7), 0);
+            Imgcodecs.imwrite("Gaussian45.png", destination);
+            BufferedImage image = ImageIO.read(new File("Gaussian45.png"));
 
-// Store the color objects in an array
+
             int w = image.getWidth();
             int h = image.getHeight();
 
-
             Color[][] colors = new Color[h][w];
-            int i = 0;
-            Color purple = new Color(42, 210, 3);
+            Color purple = new Color(210, 40, 38);
             int PURPLE = purple.getRGB();
 
-
             double max = 0;
-           double[][] lum = new double[h][w];
+            double[][] lum = new double[h][w];
             for (int j = 0; j < h; j++) {
 
                 for (int k = 0; k < w; k++) {
@@ -37,33 +45,26 @@ public class Main {
                     if(lum[j][k]>max){
                         max=lum[j][k];
                     }
-
-
-              //       System.out.print(lum[j][k]+" ");
-
-
                 }
-             //   System.out.println();
             }
             for (int j = 0; j < h; j++){
                 for(int k = 0; k < w; k++){
-                    if(lum[j][k]>(max-80)){
+                    if(lum[j][k]>110){
                         image.setRGB(k, j , PURPLE);
-
                     }
 
                 }
 
             }
-           System.out.println(max);
+            System.out.println(max);
             File output = new File("some oth.png");
             ImageIO.write(image, "png", output);
-
-
-
         } catch (IOException e) {
 
         }
-
     }
 }
+
+
+
+
